d54a4d4e69d4ad0ea37a5fff44c45cd66a325dbc
+ <+>
2020-12-22 15:03:18 -0700

    adding base methods for saving and viewing all instances

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,8 +1,17 @@
 class Brand
     attr_accessor :name
-    
+    @@all = []
+
     def intialize(name)
         @name = name
+    end
+
+    def save
+        @@all << self
+    end
+
+    def all
+        @@all
     end
 
 end
--- lib/cigar.rb
+++ lib/cigar.rb
@@ -1,7 +1,17 @@
 class Cigar
+    attr_accessor :name, :brand, :length, :gauge, :country, :strength
+    @@all = []
 
     def initialize(name)
+        @name = name
+    end
 
+    def save
+        @@all << self
+    end
+
+    def all
+        @@all
     end
 
 end
